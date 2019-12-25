a58d0c7fa11f33fe622f0167a99c8bf490bd5d0c
Rodrigo Santos <rodrigostsbc@gmail.com>
2019-12-25 17:12:26 -0300

    Primeiro Commit

---
+++ core/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Cliente
+
+# Serializers define the API representation.
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ['url', 'nome', 'endereco', 'idade']
